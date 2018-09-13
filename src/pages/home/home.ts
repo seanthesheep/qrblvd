148d1948ef3d7231aa5d656ecd9d89223fde5d20
sean sullivan <ssullivandev@gmail.com>
2018-09-13 11:58:47 -0500

    init commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AdMobFree, AdMobFreeBannerConfig } from '@ionic-native/admob-free';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public admob: AdMobFree) {
+    this.showBanner();
+  }
+  
+  showBanner() {
+    let bannerConfig: AdMobFreeBannerConfig = {
+       isTesting: true,
+       autoShow: true,
+       id: 'ca-app-pub-3940256099942544/6300978111'
+     };
+      this.admob.banner.config(bannerConfig);
+      
+      this.admob.banner.prepare().then(() => {
+      
+      }).catch(e => console.log(e));
   }
 
 }
